ed24a4b30e235cd57e9c8b3328e361a15b2227fb
nghiavt2906 <nghiavt2906@gmail.com>
2023-03-04 09:21:14 +0700

    add notification toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import { ToastContainer } from "react-toastify";
+
 import App from "./App";
 
 import store from "./store";
 
 import "bootstrap/scss/bootstrap.scss";
+import "react-toastify/scss/main.scss";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,13 +1,21 @@
 import axios from "../../config/axios";
+import toaster from "../../config/toaster";
 
 import * as types from "../action_types/user";
 
 export const loginRequest = (data) => async (dispatch) => {
-  const response = await axios.post("/auth/login", data);
+  const response = await toaster.promiseShow(
+    () => axios.post("/auth/login", data),
+    {
+      success: "Logged in sucessfully 👌",
+    }
+  );
   return dispatch({ type: types.LOGIN_SUCCESS, payload: response.data });
 };
 
 export const signupRequest = (data, navigate) => async (dispatch) => {
-  await axios.post("/auth/signup", data);
+  await toaster.promiseShow(() => axios.post("/auth/signup", data), {
+    success: "Signed up sucessfully 👌",
+  });
   navigate("/login", { replace: true });
 };
